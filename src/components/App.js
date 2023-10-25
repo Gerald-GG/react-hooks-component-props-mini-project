1f6db85d9a1613cc392612c7adecd977319a8d34
Gerald Gicharu <gicharugerald@gmail.com>
2023-10-25 08:41:09 +0300

    Completed Lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About";
+import Header from "./Header";
+import ArticleList from "./ArticleList";
+import Article from "./Article";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    This is My Blog. Call me Gee. On this, the instructions were, "You are on your own."<br />
+    React is fun and I am loving it.
+    <About image={blogData.image} />
+    <Header img={blogData.image} about={blogData.about} />
+    <Article />
+    <ArticleList />
     </div>
   );
 }
